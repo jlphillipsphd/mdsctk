0f701c8a64b5f8370b4ef0dca6e0a9eab13725dc
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2016-08-08 10:34:30 -0500

    Small fix to cleanup script.

--- sandbox/clean_build.bash
+++ sandbox/clean_build.bash
@@ -2,8 +2,31 @@
 
 ## Scripts and text files
 rm MDSCTK.bash
+rm config.h
+rm mdsctk.r
+rm Makefile
 
 ## Executables
+rm angles_to_sincos \
+   auto_decomp_sparse \
+   auto_decomp_sparse_nystrom \
+   auto_heir_decomp_sparse \
+   bb_xtc_to_phipsi \
+   ca_xtc_to_thetaphi \
+   check_xtc \
+   contact_profile \
+   decomp_dense \
+   decomp_sparse \
+   decomp_sparse_nystrom \
+   dijkstra \
+   flatten_xtc \
+   knn_data \
+   knn_data_sparse \
+   knn_rms \
+   make_sysparse \
+   make_gesparse \
+   rms_test \
+   split_xtc
 
 ## CMake files
 rm CMakeCache.txt
